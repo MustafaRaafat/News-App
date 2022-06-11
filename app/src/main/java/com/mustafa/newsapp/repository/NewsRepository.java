d9e1ceb02244a2aff5bc154c87ab3f834988d974
Mustafa Raafat <mustafa.raft@hotmail.com>
2022-06-11 16:55:18 +0200

    updated repository and viewModel with room.

--- app/src/main/java/com/mustafa/newsapp/repository/NewsRepository.java
+++ app/src/main/java/com/mustafa/newsapp/repository/NewsRepository.java
@@ -1,7 +1,13 @@
 package com.mustafa.newsapp.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.mustafa.newsapp.db.NewsDAO;
+import com.mustafa.newsapp.models.ArticlesModel;
 import com.mustafa.newsapp.models.HeadLinesModel;
 import com.mustafa.newsapp.network.NewsApiService;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -10,16 +16,28 @@ import io.reactivex.rxjava3.core.Observable;
 public class NewsRepository {
 
     private NewsApiService newsApiService;
+    private NewsDAO newsDAO;
 
-//    my personal Api key
-    private String ApiKey="78fc13aac8984c62a02ea891dcd7ff6d";
+    //    my personal Api key
+    private String ApiKey = "78fc13aac8984c62a02ea891dcd7ff6d";
 
     @Inject
-    public NewsRepository(NewsApiService newsApiService) {
+    public NewsRepository(NewsApiService newsApiService, NewsDAO newsDAO) {
         this.newsApiService = newsApiService;
+        this.newsDAO = newsDAO;
     }
 
-    public Observable<HeadLinesModel> getHeadLines(){
-        return newsApiService.getHeadLineService("eg",ApiKey);
+    public Observable<HeadLinesModel> getHeadLines() {
+        return newsApiService.getHeadLineService("eg", ApiKey);
+    }
+
+    public void insertNews(ArticlesModel articlesModel){
+        newsDAO.insertNews(articlesModel);
+    }
+    public void deletenews(int id){
+        newsDAO.deleteNews(id);
+    }
+    public LiveData<List<ArticlesModel>> getFavNews(){
+        return newsDAO.getnews();
     }
 }
--- app/src/main/java/com/mustafa/newsapp/viewModels/NewsViewModel.java
+++ app/src/main/java/com/mustafa/newsapp/viewModels/NewsViewModel.java
@@ -2,11 +2,15 @@ package com.mustafa.newsapp.viewModels;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.mustafa.newsapp.models.ArticlesModel;
 import com.mustafa.newsapp.models.HeadLinesModel;
 import com.mustafa.newsapp.repository.NewsRepository;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -16,11 +20,11 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 @HiltViewModel
 public class NewsViewModel extends ViewModel {
-    private static final String TAG="new view Model: ";
+    private static final String TAG = "new view Model: ";
 
     private NewsRepository repository;
-
-    private MutableLiveData<HeadLinesModel> headLines=new MutableLiveData<>();
+    private MutableLiveData<HeadLinesModel> headLines = new MutableLiveData<>();
+    private LiveData<List<ArticlesModel>> favorites = null;
 
     @Inject
     public NewsViewModel(NewsRepository repository) {
@@ -31,14 +35,26 @@ public class NewsViewModel extends ViewModel {
         return headLines;
     }
 
-    public void getHeadLinesFromRepository(){
+    public void getHeadLinesFromRepository() {
         repository.getHeadLines()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(headLinesModel -> {
                     headLines.setValue(headLinesModel);
-                },throwable -> {
-                    Log.d(TAG,throwable.toString());
+                }, throwable -> {
+                    Log.d(TAG, throwable.toString());
                 });
+    }
+
+    public void insertNewsFav(ArticlesModel articlesModel) {
+        repository.insertNews(articlesModel);
+    }
+
+    public void deleteFromFav(int id) {
+        repository.deletenews(id);
+    }
+
+    public void getFavNews() {
+        favorites = repository.getFavNews();
     }
 }
